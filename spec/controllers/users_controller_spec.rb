1c511e30941685fe386d3dde99917a659afbf466
Piktor19 <vktr19@gmail.com>
2011-12-14 09:05:21 +0100

    Added user microposts

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -300,5 +300,20 @@ describe UsersController do
       end
     end
   end
+  
+  describe "GET 'show'" do
     
+    before(:each) do
+      @user = Factory(:user)
+    end
+  
+    it "should show the user's microposts" do
+      mp1 = Factory(:micropost, :user => @user, :content => "Foo bar")
+      mp2 = Factory(:micropost, :user => @user, :content => "Baz quux")
+      get :show, :id => @user
+      response.should have_selector("span.content", :content => mp1.content)
+      response.should have_selector("span.content", :content => mp2.content)
+    end
+  end
+  
 end
